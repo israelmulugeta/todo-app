8c3b6504c28bd6eacade38e5a8592bdc9c842570
israelmulugeta <93257066+israelmulugeta@users.noreply.github.com>
2023-06-01 10:42:46 +0300

    adding dark mode and light mode plus task cfeation that takes into account lightmode and dark mode

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let toggler=document.querySelector('#toggler');
 toggler.addEventListener('click',toggle);
+let mode='dark';
 
 function toggle()
 {
@@ -8,6 +9,12 @@ function toggle()
         forDragDrop();
         forToggler();
         forInput();
+        tasksLightMode();
+        if(mode=='dark')
+        {
+            mode='light';
+        }
+        else mode='dark'
     }
 }
 
@@ -46,3 +53,93 @@ function forInput(){
     area.classList.toggle('searchAreaLight');
     createButton.classList.toggle('createLight');
 }
+
+
+/*
+GENERATING TASk
+
+1,make a new div give it classList='task'
+img=img.png
+text=input.value
+img2=img/xpng
+
+task.appendChild(img);
+task.appendChild(text);
+task.appendChild(img2);
+
+
+
+img.addeventListener(click,()=>{img.src='ticked.png,
+text.classList.toggle('underlinedText)})
+
+img2.addEventListener(click,()=>{delete(task)})
+
+
+
+
+STYLE
+the same as the search area both in light and dark mode
+ */
+
+
+
+
+let search=document.querySelector('.createButton');
+
+search.addEventListener('click',taskMaker);
+function taskMaker(){
+    let task =document.createElement('div')
+    let img1=new Image();
+    img1.src=`images/uncomplete.png`
+    img1.classList='forImages';
+
+    let img2=new Image();
+    img2.src=`images/delete-icon-vector-3.jpg`
+    img2.classList='forImages2'
+    
+
+    let text=document.createElement('p');
+    let input=document.querySelector('.inputField');
+    text.innerHTML=input.value;
+    text.classList='taskText'
+    input.value='';
+
+    task.appendChild(img1);
+    task.appendChild(text);
+    task.appendChild(img2);
+
+
+    if(mode=='dark')
+    {
+        task.classList='tasks';
+    }
+    else if(mode!='dark')
+    {
+        
+        task.classList='tasks';
+        task.classList.toggle('lightTasks');
+        text.classList.toggle('lightText');
+    }
+
+    
+    let taskArea=document.querySelector(".taskList");
+    taskArea.appendChild(task);
+
+}
+
+
+function tasksLightMode(){
+    
+    let myTasks=document.querySelectorAll('.tasks');
+    myTasks.forEach(task => {
+        task.classList.toggle('lightTasks');
+    });
+    
+
+    let myText=document.querySelectorAll('.taskText');
+     myText.forEach(text => {
+        text.classList.toggle('lightText');
+    });
+}
+
+
