ca95552f5abc524f5f5d78932f2d787773eaa32c
israelmulugeta <93257066+israelmulugeta@users.noreply.github.com>
2023-06-06 14:31:11 +0300

    made some changes

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
+
+
+
 let toggler=document.querySelector('#toggler');
 toggler.addEventListener('click',toggle);
 let mode='dark';
 
+
 function toggle()
 {
-    if(toggler.classList=="dark"){
+    if(mode=='dark'){
+    toggler.classList
         forBody();
         forDragDrop();
         forToggler();
         forInput();
         tasksLightMode();
+    }
         if(mode=='dark')
         {
             mode='light';
         }
-        else mode='dark'
-    }
+        else mode='dark';
+
+        localStorage.setItem('currentMode',mode);
+        console.log(mode);
+    
 }
 
 function forToggler()
@@ -54,41 +63,22 @@ function forInput(){
     createButton.classList.toggle('createLight');
 }
 
-
-/*
-GENERATING TASk
-
-1,make a new div give it classList='task'
-img=img.png
-text=input.value
-img2=img/xpng
-
-task.appendChild(img);
-task.appendChild(text);
-task.appendChild(img2);
-
-
-
-img.addeventListener(click,()=>{img.src='ticked.png,
-text.classList.toggle('underlinedText)})
-
-img2.addEventListener(click,()=>{delete(task)})
-
-
-
-
-STYLE
-the same as the search area both in light and dark mode
- */
-
-
-
-
 let search=document.querySelector('.createButton');
+let taskArea = document.querySelector('.taskList');
+let idNumber=0;
 
 search.addEventListener('click',taskMaker);
+taskArea.addEventListener('dragover', handleDragOver);
+taskArea.addEventListener('drop', handleDrop);
+
 function taskMaker(){
-    let task =document.createElement('div')
+    let task =document.createElement('li')
+    task.setAttribute("draggable", "true");
+   // task.setAttribute("ondragstart", "handleDragStart(event)");
+    task.setAttribute('id',`${idNumber}`);
+    idNumber=idNumber+1;
+    task.addEventListener('dragstart', handleDragStart);
+
     let img1=new Image();
     img1.src=`images/uncomplete.png`
     img1.classList='forImages';
@@ -121,10 +111,33 @@ function taskMaker(){
         text.classList.toggle('lightText');
     }
 
-    
-    let taskArea=document.querySelector(".taskList");
-    taskArea.appendChild(task);
 
+
+    img1.addEventListener('click',()=>
+    {
+     
+        if(img1.getAttribute('src')=='images/complete.png')
+        {
+                img1.setAttribute('src','images/uncomplete.png');
+                text.style.textDecoration='none';
+            
+        }
+        else
+        {
+                img1.setAttribute('src','images/complete.png'); 
+                text.style.textDecoration='line-through';
+
+        }
+    }
+    )
+
+
+    img2.addEventListener('click',()=>{
+        task.remove();
+    })
+
+
+    taskArea.appendChild(task);
 }
 
 
@@ -143,3 +156,25 @@ function tasksLightMode(){
 }
 
 
+//add the summary,drag and drop and local stroge then processed into react.
+
+function handleDragStart(e) {
+    e.dataTransfer.setData('text/plain', e.target.id);
+  }
+  
+  function handleDragOver(e) {
+    e.preventDefault();
+  }
+
+  function handleDrop(e) {
+    e.preventDefault();
+    const draggedId = e.dataTransfer.getData('text/plain');
+    const targetId = e.target.closest('li').id;
+    const draggedItem = document.getElementById(draggedId);
+    const targetItem = document.getElementById(targetId);
+    if (draggedId < targetId) {
+      taskArea.insertBefore(draggedItem, targetItem.nextSibling);
+    } else {
+      taskArea.insertBefore(draggedItem, targetItem.nextSibling);
+    }
+  }
